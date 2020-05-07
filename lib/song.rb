62053199dc5af436eca0a02cfcdac06d03c2703d
idith almog <almog.idith@gmail.com>
2020-05-07 22:36:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,18 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count=0
+  @@artists=[]
+  @@genres=[]
   def initialize (name,artist,genre)
     @name=name
     @artist=artist
     @genre=genre
     @@count += 1
+    if ! @@artists.include?(artist)
+      @@artists<<artist
+    end
+    if ! @@genres.include?(genre)
+      @@genres<<genre
+    end
   end
 end
